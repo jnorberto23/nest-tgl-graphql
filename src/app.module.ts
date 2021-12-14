c84584b6dc1a1fc9989556f2c6d6acd37c906b8b
jnorberto23 <joaovictor.norberto@outlook.com>
2021-12-14 14:23:47 -0300

    🎉 nest boilerplate; setting up graphql and typeorm

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { UsersModule } from './modules/users/users.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot(),
+    UsersModule,
+  ],
+  providers: [AppService],
+})
+export class AppModule {}
